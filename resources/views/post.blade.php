4c65b8d90526101243d427aa4681c58e65e2c627
Roaawan <roaafatani@outlook.com>
2023-08-06 01:27:13 +0300

    upload

---
+++ resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Roaa</title>
+
+    <link rel="stylesheet" href="/css/app.css"> 
+    <script src="/js/app.js"> </script>
+</head>
+<body>
+    <article>
+        <?=$post;?>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae explicabo iste deleniti! Accusamus neque veniam, dolorum pariatur voluptatum dolorem eum quas veritatis esse aspernatur. Numquam suscipit ea amet? Nulla.</p>
+    </article>  
+
+    <a href="/"> Go back</a>
+
+   
+
+
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Roaa</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"> </script>
+</head>
+<body>
+    <?php foreach ($posts as $post ) : ?>
+
+    <article>
+        <?= $posts;?>
+
+    </article>
+    <?php endforeach; ?>
+
+
+</body>
+</html>
