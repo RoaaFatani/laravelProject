dfec2412f08b931556ebf23bbdfac5a41feb2b57
Roaawan <roaafatani@outlook.com>
2023-08-07 20:38:43 +0300

    make post-card and post-featured-card componts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -12,18 +12,4 @@
     </article>
     <a href="/">Go Back</a>
 @endsection
-{{--<x-layout>--}}
-{{--    <article>--}}
-{{--        <h1>{{$post->title }} </h1>--}}
 
-{{--        <p>--}}
-{{--            By <a href="#"> jeffreyWay </a>in  <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>--}}
-{{--        </p>--}}
-
-{{--        <div>--}}
-{{--            {!! $post->body !!}--}}
-{{--        </div>--}}
-
-{{--        <a href="/">Go Back</a>--}}
-{{--    </article>--}}
-{{--</x-layout>--}}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,30 +1,46 @@
-@extends('components.layout')
+{{--@extends('components.layout')--}}
 
 {{--@section('banner')--}}
 {{--    <h1> home</h1>--}}
 {{--@endsection--}}
-@section('content')
-    @foreach($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{$post->slug}}">
-                    {{$post->title}}
-                </a>
-            </h1>
+{{--@section('content')--}}
+{{--    @foreach($posts as $post)--}}
+{{--        <article>--}}
+{{--            <h1>--}}
+{{--                <a href="/posts/{{$post->slug}}">--}}
+{{--                    {{$post->title}}--}}
+{{--                </a>--}}
+{{--            </h1>--}}
 
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a>in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-            </p>
+{{--            <p>--}}
+{{--                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a>in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>--}}
+{{--            </p>--}}
 
-            <div>
-                {{$post->excerpt}}
-            </div>
+{{--            <div>--}}
+{{--                {{$post->excerpt}}--}}
+{{--            </div>--}}
 
-            <div>
-                {{$post->body}}
-            </div>
-        </article>
-    @endforeach
+{{--            <div>--}}
+{{--                {{$post->body}}--}}
+{{--            </div>--}}
+{{--        </article>--}}
+{{--    @endforeach--}}
+<x-layout>
+@include('_posts-header')
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-featured-card/>
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card/>
+        <x-post-card/>
+    </div>
 
-@endsection
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card/>
+        <x-post-card/>
+        <x-post-card/>
+    </div>
+</main>
+
+{{--@endsection--}}
+</x-layout>
 
