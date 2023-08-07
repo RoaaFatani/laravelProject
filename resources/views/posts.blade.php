9d3fea4e6b191e7505c4dd7e17198bca66f663f7
Roaawan <roaafatani@outlook.com>
2023-08-07 22:13:51 +0300

    blade compont and css girds (the grids doesn't work)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -28,11 +28,20 @@
 <x-layout>
 @include('_posts-header')
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    <x-post-featured-card :post="$posts[0]"/>
-{{--    <div class="lg:grid lg:grid-cols-2">--}}
-{{--        <x-post-card/>--}}
-{{--        <x-post-card/>--}}
-{{--    </div>--}}
+{{--    @if($post->count())--}}
+        <x-post-featured-card :post="$posts[0]"/>
+{{--    @if ($posts->count()>1)--}}
+        <div class="lg:grid lg:grid-cols-6">
+            @foreach ($posts->skip(1) as $post)
+           <x-post-card :post="$post" class="col-span-2"/>
+            @endforeach
+         </div>
+{{--    @endif--}}
+{{--    @else--}}
+{{--        <p class = "text-center">--}}
+{{--            No post yet. Please check back later--}}
+{{--        </p>--}}
+{{--    @endif--}}
 
 {{--    <div class="lg:grid lg:grid-cols-3">--}}
 {{--        <x-post-card/>--}}
