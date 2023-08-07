51ff22d769fadcccddac32b863c5ecc0d53c9f70
Roaawan <roaafatani@outlook.com>
2023-08-07 06:37:22 +0300

    categoryes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,6 +11,11 @@
                     {{$post->title}}
                 </a>
             </h1>
+
+            <p>
+                <a href="#">{{$post->category->name}}</a>
+            </p>
+
             <div>
                 {{$post->excerpt}}
             </div>
