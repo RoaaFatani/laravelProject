5900372d6445abd9573e82374dac1beff491c2bd
Roaawan <roaafatani@outlook.com>
2023-08-07 20:06:22 +0300

    eager load (doesn't work)

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts',[
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category','author'])
     ]);
 
 });
@@ -42,7 +42,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
 Route::get('authors/{author:username}', function (User $author) {
 
     return view('posts',[
-        'posts' => $author->posts
+        'posts' => $author->posts->load(['category','author'])
     ]);
 
 });
