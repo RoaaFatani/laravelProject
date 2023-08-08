b8c61d7d5a63817091bb869d82e620f58aeb6730
Roaawan <roaafatani@outlook.com>
2023-08-08 16:00:32 +0300

    search bar in cleaner way

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,22 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-
-    return view('posts',[
-        'posts' => Post::latest()->with(['category','author'])->get(),
-        'categories' => Category::all()
-    ]);
-
-});
+Route::get('/',[PostController::class, 'index'])->name('home');
 
 //Route::get('posts/{post}', function ($id) {
 //    return view('/post',['post'=> $post = Post::findOrFail($id)]);
 //});
 
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('/post',['post'=> $post ]);
-});
+Route::get('posts/{post:slug}', [PostController::class,'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
 
