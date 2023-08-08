593482e9a9677e0b2dda51d5272dea2330e02cf5
Roaawan <roaafatani@outlook.com>
2023-08-08 09:31:07 +0300

    fix the css post-card

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,30 +1,3 @@
-{{--@extends('components.layout')--}}
-
-{{--@section('banner')--}}
-{{--    <h1> home</h1>--}}
-{{--@endsection--}}
-{{--@section('content')--}}
-{{--    @foreach($posts as $post)--}}
-{{--        <article>--}}
-{{--            <h1>--}}
-{{--                <a href="/posts/{{$post->slug}}">--}}
-{{--                    {{$post->title}}--}}
-{{--                </a>--}}
-{{--            </h1>--}}
-
-{{--            <p>--}}
-{{--                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a>in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>--}}
-{{--            </p>--}}
-
-{{--            <div>--}}
-{{--                {{$post->excerpt}}--}}
-{{--            </div>--}}
-
-{{--            <div>--}}
-{{--                {{$post->body}}--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    @endforeach--}}
 <x-layout>
 @include('_posts-header')
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
@@ -42,7 +15,5 @@
 {{--        <x-post-card/>--}}
 {{--    </div>--}}
 </main>
-
-{{--@endsection--}}
 </x-layout>
 
