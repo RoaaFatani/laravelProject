5217be3d2883321c734ad637fe80c6042bc027e0
Roaawan <roaafatani@outlook.com>
2023-08-13 04:14:43 +0300

    build paginateion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
         return view('posts',[
             'posts' => Post::latest()->filter(
                 request(['search'])
-            )->get(),
+            )->paginate(10),
             'categories'=> Category::all()
         ]);
     }
