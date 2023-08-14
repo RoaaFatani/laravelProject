5a4a264382a0707b09fc0e334fe685efc46013bc
Roaawan <roaafatani@outlook.com>
2023-08-14 09:24:33 +0300

    error in frist call api mailchimp

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('ping',function(){
+ $mailchimp = new \MailchimpMarketing\ApiClient();
+
+ $mailchimp->setConfig([
+     'apikey' => 'services.mailchimp.key',
+     'server' => 'us13'
+ ]);
+
+ $response = $mailchimp->ping->get();
+ ddd($response);
+});
+
 Route::get('/',[PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class,'show']);
 
